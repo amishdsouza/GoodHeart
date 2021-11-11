8235187ab2f307daa3a73162f202b5cf148e9b16
Your Name <mohitkamat99@gmail.com>
2021-11-11 20:21:05 +0530

    first

--- configs/db.php
+++ configs/db.php
@@ -1,13 +1,14 @@
 <?php
 // Database configuration
-$dbHost     = "remotemysql.com";
-$dbUsername = "Ma4SOUd77f";
-$dbPassword = "zQr3rrgFgG";
-$dbName     = "Ma4SOUd77f";
+$dbHost     = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName     = "good_heart_db";
 // Create database connection
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 // Check connection
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
 }
+
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f0066bed13df182689532dbde3003252e4aa875d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f0066bed13df182689532dbde3003252e4aa875d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
